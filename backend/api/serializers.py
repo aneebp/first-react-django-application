8709be0c73caef66a0f2c6058cbbc096e3c3f770
Aneeb <bluetar74@gmail.com>
2024-05-28 13:39:55 +0530

    backend db&serializer completed

---
+++ backend/api/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Notes
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','password']
+        extra_kwargs = {'password': {'write_only':True}}
+
+    def create(self , validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class NoteSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = "__all__"
+        extra_kwargs = {'auther': {'read_only':True}}
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer,NoteSerializers
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from .models import Notes
+
+
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class  = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializers
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = request.self.user
+        return Notes.objects.filter(auther=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(auther=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class DeleteNoteList(generics.DestroyAPIView):
+    serializer_class = NoteSerializers
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = request.self.user
+        return Notes.objects.filter(auther=user)
